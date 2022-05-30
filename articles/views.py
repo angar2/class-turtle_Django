cd66204ce0f4dd3bf8e9b0172fcce35b91ba3b93
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-05-30 21:42:24 +0900

    Feat: 템플릿 문법을 이용해서 dinner 페이지에 데이터 그리기

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,4 +6,10 @@ def index(request):
     return render(request, 'index.html')
 
 def dinner(request):
-    return render(request, 'dinner.html')
+    menus = ['족발', '햄버거', '피자', '치킨']
+    pick = random.choice(menus) # random 모듈 사용
+    context = {
+        'pick': pick,
+    } # 데이터를 넘겨줄 때는 보편적으로 context에 담아서 보냄
+    
+    return render(request, 'dinner.html', context)
