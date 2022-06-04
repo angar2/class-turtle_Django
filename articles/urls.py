711a7b55aaa915c20543013bbada7594e2749f50
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-04 16:23:55 +0900

    Fix: urls.py의 app_name과 name을 이용한 url 구분

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from articles import views
 
-
+app_name = 'articles' # 앱의 이름을 지정함
 urlpatterns = [
     path('index/', views.index),
     path('dinner/<str:name>', views.dinner), # 변수형 URL: 주소 자체를 변수처럼 사용해서 동적으로 주소를 만드는 방법 (ex. 유저 페이지)
-    path('review/', views.review),
-    path('create_review/', views.create_review),
+    path('review/', views.review, name='review'), # name: 명시한 이름으로 알아서 주소를 찾아줌
+    path('create_review/', views.create_review, name='create_review'),
 ]
     
