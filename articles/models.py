dc45a0711bb0668dc5fea27f3973d6a0100078d3
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-04 17:29:06 +0900

    Feat: articles의 Model 생성과 DB 연결

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model): # 상속: dajango에서 제공하는 Model의 설정법을 가져와서 사용
+    
+    # id는 자동으로 부여됨
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # auto_now_add: 생성 시 현재 시간을 자동 추가
