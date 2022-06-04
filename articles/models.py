a4fa7bd85f7a2c7da20263982328ef29eca56436
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-04 17:41:41 +0900

    Feat: admin 관리자 추가와 Fix: article Model 이름 변경

--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Articles(models.Model): # 상속: dajango에서 제공하는 Model의 설정법을 가져와서 사용
+class Article(models.Model): # 상속: dajango에서 제공하는 Model의 설정법을 가져와서 사용
     
     # id는 자동으로 부여됨
     title = models.CharField(max_length=10)
